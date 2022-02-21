0b21889de234607a6b84481cf84d1948649d907c
Bryan_Arias <bryanariasq02@gmail.com>
2022-02-21 11:31:47 -0500

    Add signUp

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log('hello');
+
+const signupForm = document.querySelector('#signup-form')
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#signup-email').value
+    const password = document.querySelector('#signup-password').value
+    
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            signupForm.reset();
+            //cerrar el modal
+            $('#signupModal').modal('hide');
+            console.log('signup');
+        })
+})
