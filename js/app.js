b0e6afcf3b6e199384f84d32fde54d9624b15afb
Bryan_Arias <bryanariasq02@gmail.com>
2022-03-07 15:40:37 -0500

    Add UID for each user

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
 console.log('hello');
+
+async function registrarUsuario(email, password, rol) {
+    const infoUsuario = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    ).then((usuarioFirebase) => {
+      return usuarioFirebase;
+    });
+    console.log(infoUsuario.user.uid);
+    console.log(infoUsuario);
+    
+    //const docuRef = fs.doc(fs, 'usuarios/${infoUsuario.user.uid}');
+    //fs.setDoc(docuRef, {correo: email, rol: rol});
+}
 //SignUp
 const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.querySelector('#signup-email').value
-    const password = document.querySelector('#signup-password').value
-    
-
+    const email = document.querySelector('#signup-email').value;
+    const password = document.querySelector('#signup-password').value;
+    const check = document.getElementById('toggle');
+    var rol = "Conductor";
+    if(check.checked == false){
+        rol = "Pasajero"
+    }
+    console.log(email, rol);
     emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@((amigo.edu.co))/;
     if (emailRegex.test(email)) {
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then(userCredential => {
-                signupForm.reset();
-                //cerrar el modal
-                $('#signupModal').modal('hide');
-                console.log('signup');
-            })
-       console.log('amigo.edu.co');
-       
+        registrarUsuario(email, password, rol);
+        $('#signupModal').modal('hide');
+        console.log('signup');
+        console.log('amigo.edu.co');
+        console.log(rol);
       } else {
         console.log('incorrecto');
       }
@@ -28,6 +41,27 @@ signupForm.addEventListener('submit', (e) => {
 //SignIn
 
 const signinForm = document.querySelector('#login-form');
+
+signinForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const email = document.querySelector('#login-email').value
+    const password = document.querySelector('#login-password').value
+    
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            signupForm.reset();
+            const user = userCredential.user;
+            console.log(user);
+            //cerrar el modal
+            $('#signupModal').modal('hide');
+            console.log('signIn');
+        }) 
+});
+
+//SignIn With GOOGLE
+
+const signinFor = document.querySelector('#login-form');
 
 signinForm.addEventListener('submit', e =>{
     e.preventDefault();
